2adfb808ba43f6ccf3b6ec9149454ffb477b7d44
Mochi-webdev <luke.niessen@outlook.com>
2026-05-04 21:47:02 +0200

    Artikel Feature

---
+++ ts/Articles.js
@@ -0,0 +1,50 @@
+const articles = [
+    {
+        title: "Schneegestöber statt Schulbank drücken",
+        image: "assets/Ski2026a1.jpeg",
+        text: "Am 19.03 war es so weit: Die gesamte 10. Stufe traf sich gegen 20 Uhr vor der Schule. Nachdem sich die Schüler und Schülerinnen von ihren Eltern verabschiedet hatten und der Bus fertig beladen war, ging es endlich los – 14 Stunden Abenteuer lagen vor uns.  Nach etwa 3 Stunden Fahrt, machten wir  an…",
+        link: "article1.html",
+        date: "19.03.2026"
+    },
+    {
+        title: "Von klugen Ideen und Kegelstößen – unsere SV Fahrt",
+        image: "assets/SV-Fahrt.jpg",
+        text: "Am 18. März startete eine kleine Abordnung der SV in Richtung Köln Riehl. Überraschenderweise kam, trotz des Streiks der deutschen Bahn, pünktlich um 8 Uhr unser Zug und brachte uns bis Mülheim. Von dort aus liefen wir ca. eine halbe Stunde bis zu unsererem Heim für die nächsten paar Tage…",
+        link: "article2.html",
+        date: "18.03.2026"
+    },
+    {
+        title: "5B auf der Lit.kid.Cologne!",
+        image: "assets/litkid_5b.jpg",
+        text: "Am 9. März 2026 sind wir, die Klasse 5b, mit unseren Klassenlehrer:innen Frau Sulski und Herrn Bänsch zu einer Lesung der lit.kid.COLOGNE gefahren. Die lit.kid.COLOGNE gehört zur lit.COLOGNE. Das ist ein Lesefest für jung und alt mit vielen Lesungen von morgens bis abends. Wir sind in die Lesung „Kennst du…",
+        link: "article3.html",
+        date: "09.03.2026"
+    },
+    {
+        title: "„Was einmal passiert ist, kann sich wiederholen.“  - Gedenkstättenfahrt des NCG nach Oświęcim und Krakau ",
+        image: "assets/AuschwitzfahrtTitelbild.jpg",
+        text: "Im Rahmen des in diesem Jahr erstmalig stattfindenden Projektkurses Deutsch-Geschichte „Gegen das Vergessen“, welcher sich seit den Sommerferien mit Antisemitismus und dem Holocaust beschäftigt, fuhren wir vom 21. bis zum 26. März nach Polen.  In unserem Projektkurs hatten wir uns zuvor intensiv mit der NS-Zeit beschäftigt. Dabei beleuchteten wir sowohl die Perspektive der Opfer als auch…",
+        link: "article4.html",
+        date: "04.05.2026"
+    },
+];
+
+const container = document.getElementById("articlesContainer");
+
+articles.forEach(article => {
+    const card = document.createElement("div");
+    card.classList.add("articleCard");
+
+    card.innerHTML = `
+    <img src="${article.image}" alt="${article.title}">
+    <div class="articleContent">
+        <h3>${article.title}</h3>
+        <p>${article.text.substring(0, 100)}...</p>
+        <a class="readBtn" href="${article.link}">Lesen</a>
+    </div>
+
+    <span class="articleDate">${article.date}</span>
+`;
+
+    container.appendChild(card);
+});
---
+++ ts/mainScriptLandingPage.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menuButton = document.getElementById("MenuButton");
+    const sideMenu = document.getElementById("sideMenu");
+    const overlay = document.getElementById("overlay");
+
+    function toggleMenu() {
+        menuButton.classList.toggle("active");
+        sideMenu.classList.toggle("active");
+        overlay.classList.toggle("active");
+    }
+
+    menuButton.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", toggleMenu);
+
+    document.querySelectorAll(".menuCategory").forEach(btn => {
+        btn.addEventListener("click", () => {
+            btn.parentElement.classList.toggle("open");
+        });
+    });
+
+
+    
+
+    const slides = document.querySelectorAll(".slide");
+    const dotsContainer = document.getElementById("dots");
+
+    let currentIndex = 0;
+
+    function updateSlider() {
+        slides.forEach((s, i) => s.classList.toggle("active", i === currentIndex));
+        dots.forEach((d, i) => d.classList.toggle("active", i === currentIndex));
+    }
+
+   
+    const dots = [];
+
+    slides.forEach((_, i) => {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+
+        dot.addEventListener("click", () => {
+            currentIndex = i;
+            updateSlider();
+        });
+
+        dotsContainer.appendChild(dot);
+        dots.push(dot);
+    });
+
+   
+    document.getElementById("nextBtn").addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateSlider();
+    });
+
+    document.getElementById("prevBtn").addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        updateSlider();
+    });
+
+    
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        updateSlider();
+    }, 5000);
+
+    updateSlider();
+});
