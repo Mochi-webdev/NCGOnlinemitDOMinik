7cad52b4934a80a3f3e4a69e8548f55dd0f89453
Mochi-webdev <luke.niessen@outlook.com>
2026-05-05 07:28:34 +0200

    Added Wichtige Links und Logo Click fix. ToDO: Position fix von Wichtige links und Styling

--- ts/clickNCGLOGO.js
+++ ts/clickNCGLOGO.js
@@ -1,9 +1,4 @@
 document.getElementById("NCGLogo").addEventListener("click", () => {
 
-    if (window.location.pathname.includes("Seiten/UeberUns/ArtikelTemplate.html")) {
-        window.location.href = "../../index.html";
-    }
-    else {
-        window.location.href = "index.html";
-    }
+   window.location.href = "../../index.html";
 });
