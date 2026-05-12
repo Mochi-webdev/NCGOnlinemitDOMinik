d3c5b62c8ef9d04aa98e83b706a260c3b9a8f92e
Mochi-webdev <luke.niessen@outlook.com>
2026-05-12 06:52:39 +0200

    upd admin js

--- ts/admin.js
+++ ts/admin.js
@@ -1,38 +1,25 @@
 const loginBtn = document.getElementById("loginBtn");
 
-const USER_HASH =
-"8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
 
-const PASS_HASH =
-"d7b7214ebcafdc7beccdfc1728683f95c9258c19b33a2a9f6621c2737336b010";
+const USER_HASH = "c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f";
 
+
+const PASS_HASH = "995c9a059530ffb222077555d5429a1325800fb928c82a29159cc27162d93c74";
 loginBtn.addEventListener("click", async () => {
-
-    const username =
-        document.getElementById("Username").value;
-
-    const password =
-        document.getElementById("Password").value;
+  
+    const username = document.getElementById("Username").value.trim();
+    const password = document.getElementById("Password").value.trim();
 
     const usernameHash = await hashText(username);
     const passwordHash = await hashText(password);
 
-    if (
-        usernameHash === USER_HASH &&
-        passwordHash === PASS_HASH
-    ) {
 
+    if (usernameHash === USER_HASH && passwordHash === PASS_HASH) {
         localStorage.setItem("isAdmin", "true");
-
-        window.location.href =
-        "/AdminOrdner/Admin/Dashboard.html#Admin234987s20873kl29820";
-
+        window.location.href = "/AdminOrdner/Admin/Dashboard.html#Admin234987s20873kl29820";
     } else {
-
         alert("Falscher Login");
-
     }
-
 });
 
 async function hashText(text) {
